c41a733b081c41965399055769db02fedc326e24
Vilja Hulden <vhulden@gmail.com>
2016-01-26 12:04:09 -0700

    Added another generic sscript that fixes hyphenation errors in OCR'd text

--- fix-hyphenation.py
+++ fix-hyphenation.py
@@ -4,11 +4,16 @@ Created on Tue Jul 21 18:42:43 2015
 
 @author: Vilja Hulden
 
-A script for converting a JSTOR data for research citations file (.csv) into a BibTex file (.bib) for importation into Zotero (or to any reference manager that understands BibTex, for that matter).
+A script for fixing hyphenation problems often left by OCR, i.e., this kind of
+stuff:
 
-If you do not wish to include the links to the actual files in the Zotero format, set includefiles below to 0.
+There once was a litt-
+le boy who did not really much care for his grandmo- ther.
 
-This script assumes .txt files named as they come from JSTOR Data for Research.
+The basic idea is to split the text into a list of words and for each word
+check if it ends in '-'; if it does, combine it with next word. Then check
+if the spellchecker approves the next word. If not, scratch that, and instead
+add the word without the "-" at the end.
 """
 
 import os,re
@@ -16,24 +21,24 @@ from enchant.checker import SpellChecker
 chkr = SpellChecker("en_US")
 # Change workdir and filedir to suit your needs.
 # filedir is where the full-text versions of the articles reside.
-workdir = "/Users/miki/work/research/digital/JSTOR-laborlaw/"
-filedir = "renamedbyyear/"
+workdir = ""
+filedir = "hyphproblem/"
 writedir = "hyphcorrected/"
+if not os.path.exists(writedir):
+    os.makedirs(writedir)
 
-tempfile = "/Users/miki/work/research/digital/JSTOR-laborlaw/renamedbyyear/1946_ocr_10.2307_1896734.txt"
 
 files = os.walk(workdir+filedir)
 
 for fn in files:
     filenames = fn[2]
 
-
+flag = 0
 for filename in filenames:
     with open(workdir+filedir+filename) as f:
         text = f.read()
         words = text.split()
-        
-    
+           
     newwordslist = []
     i = 0
     for word in words:
@@ -47,7 +52,7 @@ for filename in filenames:
                 flag = 1
             else:
                 newwordslist.append(word[:-1])
-        elif flag:
+        elif flag: #this is to control that we address the correct words in each iteration
             flag = 0
             i += 1
             continue
@@ -58,14 +63,5 @@ for filename in filenames:
     with open(workdir+writedir+filename, 'w') as f:
         f.write(newtext)
 
-   
-"""with open(workdir+readfile) as f:
-    entriestemp = f.read()
-    entries = [line for line in entriestemp.split('\n') if line.strip() != '']
-
-
-
-with open(workdir+writefile, 'w') as f:
-    f.write(bibentriestext)"""
     
     
